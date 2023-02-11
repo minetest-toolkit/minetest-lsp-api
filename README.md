a874f22c11397c265a4d6f7517392bb3e19896eb
luarocks <luarocks@onionmail.org>
2023-02-11 10:57:26 +0200

    Do nothing

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ These API-headers are made for [lua-language-server].
 
 Directories:
 
-* [ssm](ssm) SSM-only headers
-* [common](common) common headers between SSM and CSM
+- [ssm](ssm)-only headers
+- [common](common) headers between SSM and CSM
 
 [lua-language-server]: https://github.com/LuaLS/lua-language-server
--- common/color.lua
+++ common/color.lua
@@ -3,13 +3,13 @@
 ---------
 
 -- `#RGB` defines a color in hexadecimal format.
--- 
+--
 -- `#RGBA` defines a color in hexadecimal format and alpha channel.
--- 
+--
 -- `#RRGGBB` defines a color in hexadecimal format.
--- 
+--
 -- `#RRGGBBAA` defines a color in hexadecimal format and alpha channel.
--- 
+--
 -- Named colors are also supported and are equivalent to
 -- [CSS Color Module Level 4](https://www.w3.org/TR/css-color-4/#named-color).
 -- To specify the value of the alpha channel, append `#A` or `#AA` to the end of
@@ -27,7 +27,7 @@
 
 -- A ColorSpec specifies a 32-bit color. It can be written in any of the following
 -- forms:
--- 
+--
 -- * Table form: Each element ranging from 0..255 (a, if absent, defaults to 255):
 --   `colorspec = {a=255, r=0, g=255, b=0}`
 -- * Numerical form: The raw integer value of an ARGB8 quad.
