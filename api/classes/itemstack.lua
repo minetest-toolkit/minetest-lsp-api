25818158b837a5f4117a5bacdd72b9d2369e8bf6
luarocks <luarocks@onionmail.org>
2023-03-26 19:10:33 +0300

    Correct ItemStack
    https://github.com/minetest-toolkit/minetest-lsp-api/pull/6#issuecomment-1484092857

--- api/classes/itemstack.lua
+++ api/classes/itemstack.lua
@@ -2,6 +2,8 @@
 ---ItemStack
 ------------
 
+---@alias mt.ItemStack mt.ItemStackObject|mt.ItemString|mt.ItemStackTable|nil
+
 ---An `ItemStack` is a stack of items.
 ---
 ---It can be created via `ItemStack(x)`, where x is an ItemStack,
@@ -15,16 +17,16 @@
 ---      as stack metadata can be serialized in arbitrary order.
 ---* Note: if `stack2` is an itemstring or table representation of an
 ---      ItemStack, this will always return false, even if it is "equivalent".
----@class mt.ItemStack
-local ItemStackClass = {}
+---@class mt.ItemStackObject
+local ItemStackObject = {}
 
 ---`ItemStack` constructor.
----@param x mt.ItemStack|mt.ItemString|mt.ItemStackTable|nil
----@return mt.ItemStack
+---@param x mt.ItemStack
+---@return mt.ItemStackObject
 function ItemStack(x) end
 
 ---@class mt.ItemStackTable
----@field meta {[string]: number|string}
+---@field meta {[string]: string}
 ---@field metadata string deprecated
 ---@field count integer
 ---@field name string
@@ -33,44 +35,44 @@ local ItemStackTable = {}
 
 ---Returns `true` if stack is empty.
 ---@return boolean
-function ItemStackClass:is_empty() end
+function ItemStackObject:is_empty() end
 
 ---@return string name ie: "default:stone"
-function ItemStackClass:get_name() end
+function ItemStackObject:get_name() end
 
 ---Returns a boolean indicating whether the item was cleared.
 ---@param item_name string
 ---@return boolean cleared
-function ItemStackClass:set_name(item_name) end
+function ItemStackObject:set_name(item_name) end
 
 ---Returns number of items on the stack.
 ---@return number
-function ItemStackClass:get_count() end
+function ItemStackObject:get_count() end
 
 ---Returns a boolean indicating whether the item was cleared.
 ---@param count integer Unsigned 16 bit.
 ---@return boolean cleared
-function ItemStackClass:set_count(count) end
+function ItemStackObject:set_count(count) end
 
 ---Returns tool wear (`0`-`65535`), `0` for non-tools.
 ---@return number
-function ItemStackClass:get_wear() end
+function ItemStackObject:get_wear() end
 
 ---Returns boolean indicating whether item was cleared.
 ---@param wear integer Unsigned 16 bit.
 ---@return boolean cleared
-function ItemStackClass:set_wear(wear) end
+function ItemStackObject:set_wear(wear) end
 
 ---**DEPRECATED** Returns metadata (a string attached to an item stack).
 ---@return string
 ---@deprecated
-function ItemStackClass:get_metadata() end
+function ItemStackObject:get_metadata() end
 
 ---**DEPRECATED**
 ---@param metadata string
 ---@return true
 ---@deprecated
-function ItemStackClass:set_metadata(metadata) end
+function ItemStackObject:set_metadata(metadata) end
 
 ---* Returns the description shown in inventory list tooltips.
 ---* The engine uses this when showing item descriptions in tooltips.
@@ -79,7 +81,7 @@ function ItemStackClass:set_metadata(metadata) end
 ---  * `description` in item definition;
 ---  * item name.
 ---@return string
-function ItemStackClass:get_description() end
+function ItemStackObject:get_description() end
 
 ---* Returns the short description or nil.
 ---* Unlike the description, this does not include new lines.
@@ -89,76 +91,76 @@ function ItemStackClass:get_description() end
 ---  * first line of the description (From item meta or def, see `get_description()`);
 ---  * Returns nil if none of the above are set.
 ---@return string|nil
-function ItemStackClass:get_short_description() end
+function ItemStackObject:get_short_description() end
 
 ---Removes all items from the stack, making it empty.
-function ItemStackClass:clear() end
+function ItemStackObject:clear() end
 
 ---Replace the contents of this stack.
 ---@param item string|table
-function ItemStackClass:replace(item) end
+function ItemStackObject:replace(item) end
 
 ---Returns the stack in itemstring form.
 ---@return string
-function ItemStackClass:to_string() end
+function ItemStackObject:to_string() end
 
 ---Returns the stack in Lua table form.
 ---@return table
-function ItemStackClass:to_table() end
+function ItemStackObject:to_table() end
 
 ---Returns the maximum size of the stack (depends on the item).
 ---@return number
-function ItemStackClass:get_stack_max() end
+function ItemStackObject:get_stack_max() end
 
 ---Returns `get_stack_max() - get_count()`.
 ---@return number
-function ItemStackClass:get_free_space() end
+function ItemStackObject:get_free_space() end
 
 ---Returns `true` if the item name refers to a defined item type.
 ---@return boolean
-function ItemStackClass:is_known() end
+function ItemStackObject:is_known() end
 
 ---Returns the item definition table.
 ---@return table
-function ItemStackClass:get_definition() end
+function ItemStackObject:get_definition() end
 
 ---Returns the digging properties of the item, or those of the hand if none are
 ---defined for this item type.
 ---@return mt.ToolCaps
-function ItemStackClass:get_tool_capabilities() end
+function ItemStackObject:get_tool_capabilities() end
 
 ---Increases wear by `amount` if the item is a tool, otherwise does nothing
 ---@param amount integer 0..65536
-function ItemStackClass:add_wear(amount) end
+function ItemStackObject:add_wear(amount) end
 
 ---* Increases wear in such a way that, if only this function is called,
 ---  the item breaks after `max_uses` times.
 ---* Does nothing if item is not a tool or if `max_uses` is 0.
 ---@param max_uses integer 0..65536
-function ItemStackClass:add_wear_by_uses(max_uses) end
+function ItemStackObject:add_wear_by_uses(max_uses) end
 
 ---* Returns leftover `ItemStack`.
 ---* Put some item or stack onto this stack.
----@param item mt.ItemStack
----@return mt.ItemStack leftover
-function ItemStackClass:add_item(item) end
+---@param item mt.ItemStackObject
+---@return mt.ItemStackObject leftover
+function ItemStackObject:add_item(item) end
 
 ---Returns `true` if item or stack can be fully added to this one.
----@param item mt.ItemStack
+---@param item mt.ItemStackObject
 ---@return boolean
-function ItemStackClass:item_fits(item) end
+function ItemStackObject:item_fits(item) end
 
 ---* Returns taken `ItemStack`.
 ---* Take (and remove) up to `n` items from this stack.
 ---@param n number|nil Default: `1`.
----@return mt.ItemStack taken
-function ItemStackClass:take_item(n) end
+---@return mt.ItemStackObject taken
+function ItemStackObject:take_item(n) end
 
 ---* Returns taken `ItemStack`.
 ---* Copy (don't remove) up to `n` items from this stack.
 ---@param n number|nil Default: `1`.
----@return mt.ItemStack taken
-function ItemStackClass:peek_item(n) end
+---@return mt.ItemStackObject taken
+function ItemStackObject:peek_item(n) end
 
 ---* Returns `true` if this stack is identical to `other`.
 ---* Note: `stack1:to_string() == stack2:to_string()` is not reliable,
@@ -166,5 +168,5 @@ function ItemStackClass:peek_item(n) end
 ---* Note: if `other` is an itemstring or table representation of an
 ---  ItemStack, this will always return false, even if it is
 ---  "equivalent".
----@param other mt.ItemStack
-function ItemStackClass:equals(other) end
+---@param other mt.ItemStackObject
+function ItemStackObject:equals(other) end
