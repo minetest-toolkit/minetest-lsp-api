ccbb801b9bc592f6648355fca008da78d9ab3cb8
luarocks <luarocks@onionmail.org>
2023-04-14 20:23:48 +0300

    Add constants

--- api/minetest/minetest.lua
+++ api/minetest/minetest.lua
@@ -6,7 +6,20 @@
 ---* [Developer Wiki](http://dev.minetest.net)
 ---* [Unofficial Modding Book](https://rubenwardy.com/minetest_modding_book)
 ---@class mt.Core
-minetest = {}
+minetest = {
+  CONTENT_AIR = 126,
+  CONTENT_IGNORE = 127,
+  CONTENT_UNKNOWN = 125,
+  EMERGE_CANCELLED = 0,
+  EMERGE_ERRORED = 1,
+  EMERGE_FROM_DISK = 3,
+  EMERGE_FROM_MEMORY = 2,
+  EMERGE_GENERATED = 4,
+  LIGHT_MAX = 14,
+  MAP_BLOCKSIZE = 16,
+  PLAYER_MAX_BREATH_DEFAULT = 10,
+  PLAYER_MAX_HP_DEFAULT = 20,
+}
 
 ---@deprecated
 ---@type mt.Core
